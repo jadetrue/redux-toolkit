d0b4f013af5acae0cfc717149df0b69469748a9a
Jade True <jade.true@moneyhub.com>
2023-01-03 16:56:56 +0000

    bind action creators - useful

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const redux = require("redux");
 const createStore = redux.createStore;
+const bindActionCreators = redux.bindActionCreators;
 
 const CAKE_ORDERED = "CAKE_ORDERED";
 const CAKE_RESTOCKED = "CAKE_RESTOCKED";
@@ -53,10 +54,16 @@ const unsubscribe = store.subscribe(() =>
 );
 
 // Reducer will return based on the stock that's changed in the listener
-store.dispatch(orderCake());
-store.dispatch(orderCake());
-store.dispatch(orderCake());
+// store.dispatch(orderCake());
+// store.dispatch(orderCake());
+// store.dispatch(orderCake());
 
-store.dispatch(restockCake(3));
+// store.dispatch(restockCake(3));
+
+const actions = bindActionCreators({ orderCake, restockCake }, store.dispatch);
+actions.orderCake();
+actions.orderCake();
+actions.orderCake();
+actions.restockCake(3);
 
 unsubscribe();
